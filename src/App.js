46030822405a03f973851145979db754108d7300
Roman Kuite <roman.kuite@gmail.com>
2021-02-11 15:50:49 +0100

    Cleaned up project and only added post view mode

--- src/App.js
+++ src/App.js
@@ -2,116 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import BlogEntry from "./components/BlogEntry";
 import {Button} from "./components/FancyButton";
-import React, { useState } from 'react';
 import AddStudentForm from "./components/AddStudentForm";
 import {getAdressFromStudents, getStudentsFromApi} from "./services/getStudentsFromApi";
 import axios from "axios";
 import StudentEntry from "./components/StudentEntryAPI";
+import React, { useState, useEffect } from 'react';
 
 function App() {
-    // Variables
-    // const firstStudent = "Horst";
-    const [students, setStudents] = useState([]);
-    const [studentsAPI, setStudentsAPI] = useState([]);
 
-    // const axiosResponse = getStudentsFromApi().then(response => { console.log(response.data); setStudents(response.data)})
+    const [studentsAPI, setStudentsAPI] = useState({});
+    const [selectedStudentID, setSelectedStudentID] = useState(1);
+
     function loadStudentsFromApi(){
-    getStudentsFromApi().then(data => setStudentsAPI([...students, ...data]));
+        getStudentsFromApi().then(data => setStudentsAPI(data));
     }
 
-    // const students = [
-    //     {
-    //         id: "1",
-    //         name: "Franky",
-    //         university: "Trier"
-    //     },
-    //     {
-    //         id: "2",
-    //         name: "Franzi",
-    //         university: "Bochum"
-    //     },
-    //     {
-    //         id: "3",
-    //         name: "Maria",
-    //         university: "Hamburg"
-    //     },
-    //     {
-    //         id: "4",
-    //         name: "Horst",
-    //         university: "Grevenbroich"
-    //     }
-    // ]
+    /////////////////////////////////////////////////////////
+
+    useEffect(()=> {
+    axios.get("https://jsonplaceholder.typicode.com/users/"+selectedStudentID)
+        .then(response => setStudentsAPI(response.data))
+    },[selectedStudentID])
+
+
+    useEffect(()=> {
+        loadStudentsFromApi();
+    }, [])
+
+    /////////////////////////////////////////////////////////
 
     return (
         // Alles in ein TAG! --> Fragment REACT -> LOOK IT UP
         <React.Fragment>
             <h2 className={"multi"}>Gesichtsbuch 2.0</h2>
                 <div>
-                    {students.map(student => <BlogEntry key={student.id}
-                                                        id={"ID: " + student.id}
-                                                        name={"Name: " + student.name}
-                                                        university={"Uni: " + student.university}
-                                                        onDelete={()=>
-                                                            { const updatedList = students.filter(item => item.id !== student.id);
-                                                            setStudents(updatedList);
-                                                            }
-                                                        }/>)
-                    }
 
-                    {studentsAPI.map(studentAPI => <StudentEntry key={studentAPI.id}
-                                                        id={"ID: " + studentAPI.id}
-                                                        name={"Name: " + studentAPI.name}
-                                                        email={"Email: " + studentAPI.email}
-                                                        phone={"phone: " + studentAPI.phone}
-                                                        adress={"Adress: " + studentAPI.adress}
-                                                                 onDelete={()=>
-                                                        { const updatedList = studentsAPI.filter(item => item.id !== studentAPI.id);
-                                                            setStudentsAPI(updatedList);
-                                                        }
-                                                        }/>)
-                    }
 
-                    <Button primary onClick={() => setStudents([
-                            {
-                                id: "1",
-                                name: "Franky",
-                                university: "Trier"
-                            },
-                            {
-                                id: "2",
-                                name: "Franzi",
-                                university: "Bochum"
-                            },
-                            {
-                                id: "3",
-                                name: "Maria",
-                                university: "Hamburg"
-                            },
-                            {
-                                id: "4",
-                                name: "Horst",
-                                university: "Grevenbroich"
-                            }
-                        ])}>SPIN 'EM IN!
-                    </Button>
 
-                    <AddStudentForm onAdd={
-                        newStudent => setStudents([...students, newStudent])
-                    }> Add new human! </AddStudentForm>
 
-                    <Button onClick={ () => loadStudentsFromApi()}> Get Students From API </Button>
+                    <Button onClick={ () => setSelectedStudentID(selectedStudentID + 1)}> Next ID </Button>
 
-                    {/*<Button primary onClick={() => {*/}
-                    {/*    const updatedList = [*/}
-                    {/*        ...students,*/}
-                    {/*        {id: "new-id",*/}
-                    {/*        name: "new student",*/}
-                    {/*        university: "super uni"}*/}
-                    {/*        ];*/}
-                    {/*    setStudents(updatedList);*/}
-                    {/*}*/}
-                    {/*}> Add new human!*/}
-                    {/*</Button>*/}
+                    <StudentEntry email={studentsAPI.email} id={studentsAPI.id} name={studentsAPI.name} />
+
 
 
                 </div>
